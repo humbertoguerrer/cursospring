2bb923c18876a73c60cb16864e5de7c97885439b
humbertoguerrer <humberto.guerrer@gmail.com>
2020-12-14 23:37:19 -0300

    Ajustes do endpoint/categorias/{id}

--- src/main/java/com/nelioalves/cursospring/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursospring/domain/Categoria.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 public class Categoria implements Serializable{
@@ -29,6 +31,7 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
