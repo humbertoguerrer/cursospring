efa467b033d071d561e4f3751e31f8e3ab7fbf37
humbertoguerrer <humberto.guerrer@gmail.com>
2020-12-20 23:30:53 -0300

    Implementando anotações spring na classe ItemPedido

--- src/main/java/com/nelioalves/cursospring/CursoSpringApplication.java
+++ src/main/java/com/nelioalves/cursospring/CursoSpringApplication.java
@@ -13,6 +13,7 @@ import com.nelioalves.cursospring.domain.Cidade;
 import com.nelioalves.cursospring.domain.Cliente;
 import com.nelioalves.cursospring.domain.Endereco;
 import com.nelioalves.cursospring.domain.Estado;
+import com.nelioalves.cursospring.domain.ItemPedido;
 import com.nelioalves.cursospring.domain.Pagamento;
 import com.nelioalves.cursospring.domain.PagamentoComBoleto;
 import com.nelioalves.cursospring.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.nelioalves.cursospring.repositories.CidadeRepository;
 import com.nelioalves.cursospring.repositories.ClienteRepository;
 import com.nelioalves.cursospring.repositories.EnderecoRepository;
 import com.nelioalves.cursospring.repositories.EstadoRepository;
+import com.nelioalves.cursospring.repositories.ItemPedidoRepository;
 import com.nelioalves.cursospring.repositories.PagamentoRepository;
 import com.nelioalves.cursospring.repositories.PedidoRepository;
 import com.nelioalves.cursospring.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursoSpringApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -111,6 +115,19 @@ public class CursoSpringApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
 	}
 
 }
--- src/main/java/com/nelioalves/cursospring/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursospring/domain/Pedido.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,6 +33,7 @@ public class Pedido {
 	@JoinColumn(name = "endereco_entrega_id")
 	private Endereco enderecoDeEntrega;
 
+	@OneToMany(mappedBy = "id.pedido")
 	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
