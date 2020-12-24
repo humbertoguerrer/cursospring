2aa8691d4b73556d31d8dac5c970da086ee7ff2e
humbertoguerrer <humberto.guerrer@gmail.com>
2020-12-24 00:52:20 -0300

    Validação sintática com Beans Validation

--- src/main/java/com/nelioalves/cursospring/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursospring/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.cursospring.DTO.CategoriaDTO;
 import com.nelioalves.cursospring.domain.Categoria;
 import com.nelioalves.cursospring.repositories.CategoriaRepository;
 import com.nelioalves.cursospring.services.exceptions.DataIntegrityException;
@@ -49,9 +50,13 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
